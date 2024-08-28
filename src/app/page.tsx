bfa6d85e2d760fc5716d8dceba7a07abeda6feb1
Queenijain <jainqueeni@gmail.com>
2024-08-28 16:43:29 +0530

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,23 @@
+import FeaturedCourses from "@/components/FeaturedCourses";
+import Footer from "@/components/Footer";
+import HeroSec from "@/components/HeroSec";
+import Instructors from "@/components/Instructors";
+import MusicSchoolTestimonial from "@/components/TestimonialCards";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
+import WhyChooseUs from "@/components/WhyChooseUs";
+import { Main } from "next/document";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+      <HeroSec/>
+      <FeaturedCourses/>
+      <WhyChooseUs/>
+      <MusicSchoolTestimonial/>
+      <UpcomingWebinars/>
+      <Instructors/>
+      <Footer/>
+    </main>
+  );
+}
---
+++ src/components/WhyChooseUs.tsx
@@ -0,0 +1,82 @@
+"use client";
+import React from "react";
+import { StickyScroll } from "./ui/sticky-scroll-reveal";
+import Image from "next/image";
+
+
+
+const musicSchoolContent = [
+
+    {
+      title: 'Discover Your Sound with Us: A Personal Journey in Music Mastery',
+      description:
+        'Embark on a musical journey that’s uniquely yours. Our personalized instruction adapts to your individual needs, setting the stage for unparalleled growth and creativity. At our music school, your aspirations meet our dedicated support, creating a harmonious path to mastery.',
+        content: (
+          <div className="h-full w-full  flex items-center justify-center text-white">
+            <Image
+              src="https://images.unsplash.com/photo-1514320291840-2e0a9bf2a9ae?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fG11c2ljfGVufDB8fDB8fHww"
+              width={300}
+              height={300}
+              className="h-full w-full object-cover"
+              alt="linear board demo"
+            />
+          </div>
+        ),
+    },
+    {
+      title: 'Live Feedback & Engagement',
+      description:
+        'Immerse yourself in an interactive learning experience where feedback is immediate, just like real-time changes in a collaborative project. This approach enhances your understanding and mastery of music concepts and performance techniques.',
+        content: (
+          <div className="h-full w-full  flex items-center justify-center text-white">
+            <Image
+              src="https://images.unsplash.com/photo-1484383564173-89117c57b303?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8bXVzaWMlMjBzY2hvb2x8ZW58MHx8MHx8fDA%3D"
+              width={300}
+              height={300}
+              className="h-full w-full object-cover"
+              alt="linear board demo"
+            />
+          </div>
+        ),
+    },
+    {
+      title: 'Cutting-Edge Curriculum',
+      description:
+        'Our curriculum is continuously updated to include the latest music education trends and technologies, ensuring you’re always learning with the most current and effective methods. Say goodbye to outdated materials and welcome an education that evolves with the industry.',
+        content: (
+          <div className="h-full w-full  flex items-center justify-center text-white">
+            <Image
+              src="https://images.unsplash.com/photo-1445985543470-41fba5c3144a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8bXVzaWN8ZW58MHx8MHx8fDA%3D"
+              width={300}
+              height={300}
+              className="h-full w-full object-cover"
+              alt="linear board demo"
+            />
+          </div>
+        ),
+    },
+    {
+      title: 'Limitless Learning Opportunities',
+      description:
+        'With our expansive resource library and dynamic course offerings, you’ll never find yourself without something new to explore. Our platform provides continuous opportunities for growth, ensuring your musical skills are always advancing.',
+        content: (
+          <div className="h-full w-full  flex items-center justify-center text-white">
+            <Image
+              src="https://images.unsplash.com/photo-1470225620780-dba8ba36b745?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bXVzaWN8ZW58MHx8MHx8fDA%3D"
+              width={300}
+              height={300}
+              className="h-full w-full object-cover"
+              alt="linear board demo"
+            />
+          </div>
+        ),
+    },
+  ];
+
+function WhyChooseUs() {
+  return (<div>
+    <StickyScroll content={musicSchoolContent} />
+  </div>)
+}
+
+export default WhyChooseUs;
